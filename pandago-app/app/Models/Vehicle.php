009b9071980a92fbd75dbef1020acfc60d46c65a
DanielPeralta <dani.peralta.de@gmail.com>
2022-12-26 17:05:31 +0100

    Create all models and added relationships many to many

--- pandago-app/app/Models/Vehicle.php
+++ pandago-app/app/Models/Vehicle.php
@@ -15,4 +15,29 @@ class Vehicle extends Model
      * @var string
      */
     protected $table = 'vehicle';
+
+    /**
+     * Get all the uses for the vehicle.
+     */
+    public function uses()
+    {
+        return $this->morphToMany(UseV::class, 'vehicle_use');
+    }
+
+    /**
+     * Get all the requirements for the vehicle.
+     */
+    public function requirements()
+    {
+        return $this->morphToMany(Requirement::class, 'vehicle_requirements');
+    }
+
+    /**
+     * Get all the client types for the vehicle.
+     */
+    public function client_types()
+    {
+        return $this->morphToMany(ClientType::class, 'vehicle_client_type');
+    }
+
 }
